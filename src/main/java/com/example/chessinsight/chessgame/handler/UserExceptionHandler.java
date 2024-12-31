a515181afb97ff967ce21cf22d9cc1f6b9d64e8a
정민규 <fastdodge7@naver.com>
2024-12-31 09:55:25 +0900

    [feat] UserExceptionHandler의 BasePackages를 UserController가 있는 패키지로 제한

--- src/main/java/com/example/chessinsight/chessgame/handler/UserExceptionHandler.java
+++ src/main/java/com/example/chessinsight/chessgame/handler/UserExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "com.example.chessinsight.chessgame.controlle")
 public class UserExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
